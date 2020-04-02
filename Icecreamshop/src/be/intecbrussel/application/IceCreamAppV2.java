59754d3348318accf5dd6f3171c5de1353295dce
shushan <shushan@DESKTOP-2KS6E60.home>
2020-04-03 00:37:17 +0200

    icecreamcar

--- Icecreamshop/src/be/intecbrussel/application/IceCreamAppV2.java
+++ Icecreamshop/src/be/intecbrussel/application/IceCreamAppV2.java
@@ -9,12 +9,14 @@ import be.intecbrussel.sellers.Stock;
 public class IceCreamAppV2 {
 
 	public static void main(String[] args) {
-		
-		IceCreamSeller iceCreamCar=new IceCreamCar(new PriceList(1.2, 1.00, 1.25), new Stock(3, 4, 2, 5));
+
+		IceCreamSeller iceCreamCar = new IceCreamCar(new PriceList(1.2, 1.00, 1.25), new Stock(3, 4, 2, 5));
 		iceCreamCar.orderIceRocket();
+		iceCreamCar.orderIceRocket();
+		iceCreamCar.orderIceRocket();
+		iceCreamCar.orderIceRocket();
+
 		System.out.println(iceCreamCar);
-		
-		
 
 	}
 
--- Icecreamshop/src/be/intecbrussel/sellers/IceCreamCar.java
+++ Icecreamshop/src/be/intecbrussel/sellers/IceCreamCar.java
@@ -23,54 +23,69 @@ public class IceCreamCar implements IceCreamSeller {
 	}
 
 	@Override
-	public Cone orderCone(Flavor[] balls)  {
+	public Cone orderCone(Flavor[] balls) {
 
 		return prepareCone(balls);
 	}
 
-	private Cone prepareCone(Flavor[] balls)  {
+	private Cone prepareCone(Flavor[] balls) {
 		Cone Cone = new Cone(balls);
-		if (stock.getCones() > 0 && stock.getBalls() > balls.length) {
-		
-			profit += priceList.getBallPrice()*balls.length;
-			
-		} else {
+		try {
+			if (stock.getCones() > 0 && stock.getBalls() > balls.length) {
+
+				profit += priceList.getBallPrice() * balls.length;
+				stock.setCones(stock.getCones() - 1);
+				stock.setBalls(stock.getBalls() - 1);
+			}
+		} catch (Exception e) {
 			System.out.println("no more ice cream in the stock");
 		}
 		return Cone;
 	}
 
 	@Override
-	public IceRocket orderIceRocket()  {
+	public IceRocket orderIceRocket() {
 
 		return prepareRocket();
 	}
 
-	private IceRocket prepareRocket() {
+	private IceRocket prepareRocket()  {
 		IceRocket newIceRocket = new IceRocket();
+
 		if (stock.getIceRockets() > 0) {
 			profit += priceList.getRocketPrice();
-			
-		} else {
-			System.out.println("no more ice rocket");
+			stock.setIceRockets(stock.getIceRockets() - 1);
+
 		}
+
+		else {
+			System.out.println("no more ice rocket in the stock");
+		}
+
 		return newIceRocket;
+
 	}
 
 	@Override
-	public Magnum orderMagnum(MagnumType type)	 {
+	public Magnum orderMagnum(MagnumType type) {
 
 		return prepareMagnum(type);
 	}
 
-	private Magnum prepareMagnum(MagnumType type) 	 {
+	private Magnum prepareMagnum(MagnumType type) {
 		Magnum magnum = new Magnum(type);
-		if (stock.getMagni() > 0) {
-			profit += priceList.getMagnumStandardPrice() * type.getPrice();
-		
-		} else {
+		try {
+			if (stock.getMagni() > 0) {
+				profit += priceList.getMagnumStandardPrice() * type.getPrice();
+				stock.setMagni(stock.getMagni() - 1);
+			}
+		} catch (Exception e) {
 			System.out.println("no more magnum type in stock");
+
 		}
+//		else {
+//			System.out.println("no more magnum type in stock");
+//		}
 		return magnum;
 	}
 
@@ -84,6 +99,5 @@ public class IceCreamCar implements IceCreamSeller {
 	public String toString() {
 		return "IceCreamCar [priceList=" + priceList + ", stock=" + stock + ", profit=" + profit + "]";
 	}
-	
 
 }
