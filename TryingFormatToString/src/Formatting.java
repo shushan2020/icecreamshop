b6da7c43cf35868a4ae707df64540adb5dbfcd12
shushan <shushan@DESKTOP-2KS6E60.home>
2020-05-01 16:14:49 +0200

    trying

---
+++ TryingFormatToString/src/Formatting.java
@@ -0,0 +1,20 @@
+
+public class Formatting {
+public static void main(String[] args) {
+
+			     String title,author;
+			     int numberOfPages;
+
+			    
+			       title = "java";
+			       author = "sirisha";
+			       numberOfPages = 600;
+			    
+
+			  	
+			        System.out.format("%-10s%-10s%10dkg", title, author, numberOfPages);
+			    }
+
+		
+
+}
